4d57e44247169b655c827e60e5706db3890cc372
Kaan3223 <kaanaygar3223@gmail.com>
2023-03-20 18:52:29 -0500

    created sort functionality for pricing low to high, high to low and rating

--- books.js
+++ books.js
@@ -1,24 +1,53 @@
-function renderBooks() {
+function renderBooks(filter) {
     const booksWrapper = document.querySelector('.books');
-    booksWrapper.innerHTML = 
-    `<div class="book">
+    const books = getBooks();
+    
+
+    if (filter === 'LOW_TO_HIGH') {
+        books.sort((a, b) => a.originalPrice - b.originalPrice);
+    }
+    else if (filter === 'HIGH_TO_LOW') {
+        books.sort((a, b) => b.originalPrice - a.originalPrice);
+    }
+    else if (filter === 'RATING') {
+        books.sort((a, b) => b.rating - a.rating);
+    }
+
+    let ratingHtml = '';
+
+    for (let i = 0; i < Math.floor(4); ++i) {
+        ratingHtml += '<i class = "fas fa-star"></i>'
+    }
+
+    console.log(ratingHtml);
+
+    const booksHtml = books.map(book => {
+        return`<div class="book">
             <figure class="book__img--wrapper">
-              <img class="book__img" src="assets/atomic habits.jpg" alt="">
+              <img class="book__img" src="${book.url}" alt="">
             </figure>
             <div class="book__title">
-              Atomic Habits
+              ${book.title}
             </div>
             <div class="book__ratings">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star-half-alt"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
             </div>
             <div class="book__price">
-              <span class="book__price--normal">$59.95</span> $14.95
+              <span class="">$${book.originalPrice.toFixed(2)}</span>
             </div>
         </div>`
+    })
+    .join("");
+
+    booksWrapper.innerHTML = booksHtml;
+ }
+
+ function filterBooks(event) {
+    renderBooks(event.target.value);
+    
 }
 
 setTimeout(() => {
